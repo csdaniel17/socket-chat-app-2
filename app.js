764e6583696fe5ea7f737cb4ecb1bcf7db92707e
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-06-28 11:24:01 -0400

    add comments

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http); //web socket has to work with http
 
 app.get('/', function(req, res) {
   // res.send('<h1>Hello World</h1>');
-  res.sendfile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
+  // console.log('a user connected');
+  // socket.on('disconnect', function() {
+  //   console.log('user disconnected');
+  // });
+  //to send a message to everyone except a certain socket
+  // socket.broadcast.emit('hi');
+
+  //listen for chat messages
   socket.on('chat message', function(msg) {
-    console.log('message: ' + msg);
+    // console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
 });
